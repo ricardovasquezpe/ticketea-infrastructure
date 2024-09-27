92cacf0f104acc6ece3485a94d416e967a5441c0
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-09-27 17:42:54 -0500

    fix

--- ticketea-userdata.sh
+++ ticketea-userdata.sh
@@ -11,3 +11,7 @@ docker login -u ricardovasquezpe -p Ajinomoto123@
 curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # docker-compose --version
+
+git clone https://github.com/ricardovasquezpe/ticketea-infrastructure.git
+cd ticketea-infrastructure
+docker-compose up -d
