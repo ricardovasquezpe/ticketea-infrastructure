62a4efdd1efe2d4ac7b9d6c3fe058e2a8f6ad892
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-10-01 16:37:53 -0500

    fix

--- main.tf
+++ main.tf
@@ -15,63 +15,13 @@ locals {
   instance_ami    = "ami-00eb69d236edcfaf8"
   public_key_path = "${path.module}/ticketea-key.pub"
   userdata_path   = "${path.module}/ticketea-userdata.sh"
+  aws_region      = "us-east-2"
 }
 
 provider "aws" {
-  region     = "us-east-2"
+  region     = local.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 provider "docker" {}
-
-/*resource "aws_key_pair" "ticketea_key" {
-  key_name   = "ticketea_key"
-  public_key = file(local.public_key_path)
-}
-
-resource "aws_security_group" "ticketea_backend_security_group" {
-  name        = "ticketea_backend_security_group"
-  description = "Allow traffic"
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "ticketea_instance" {
-  ami             = local.instance_ami
-  security_groups = [aws_security_group.ticketea_backend_security_group.name]
-  instance_type   = "t2.micro"
-  key_name        = aws_key_pair.ticketea_key.key_name
-  user_data       = file(local.userdata_path)
-
-  depends_on = [null_resource.push_to_dockerhub]
-}
-
-resource "aws_eip" "ticketea_elastic_ip" {
-  vpc = false
-}
-
-resource "aws_eip_association" "ticketea_elastic_ip_association" {
-  instance_id   = aws_instance.ticketea_instance.id
-  allocation_id = aws_eip.ticketea_elastic_ip.id
-}
-*/
