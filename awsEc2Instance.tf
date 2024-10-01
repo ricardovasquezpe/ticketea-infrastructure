9cf0ac10d3f20b0125909ba4d556e3f25c126e39
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-10-01 18:47:23 -0500

    fix

--- awsEc2Instance.tf
+++ awsEc2Instance.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "ticketea_backend_security_group" {
   }
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
