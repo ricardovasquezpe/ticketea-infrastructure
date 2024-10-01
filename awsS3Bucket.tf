30776918c13bec26cc98273635f6a1261eea79a5
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-10-01 11:55:36 -0500

    fix

--- awsS3Bucket.tf
+++ awsS3Bucket.tf
@@ -3,9 +3,9 @@ resource "aws_s3_bucket" "ticketea_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "ticketea_bucket_acl" {
-    bucket = aws_s3_bucket.ticketea_bucket.id
-    acl    = "public-read"
-    depends_on = [aws_s3_bucket_ownership_controls.ticketea_bucket_ownership]
+  bucket     = aws_s3_bucket.ticketea_bucket.id
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.ticketea_bucket_ownership]
 }
 
 resource "aws_s3_bucket_ownership_controls" "ticketea_bucket_ownership" {
@@ -26,23 +26,42 @@ resource "aws_s3_bucket_public_access_block" "ticketea_bucket_allow_public_acces
 }
 
 resource "aws_s3_bucket_policy" "ticketea_bucket_policy" {
-    bucket = aws_s3_bucket.ticketea_bucket.id
-    policy = jsonencode({
+  bucket = aws_s3_bucket.ticketea_bucket.id
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "PublicReadGetObject"
+        Sid       = "PublicReadGetObject"
         Principal = "*"
         Action = [
           "s3:GetObject"
         ]
-        Effect   = "Allow"
+        Effect = "Allow"
         Resource = [
           "arn:aws:s3:::${var.s3_bucket_name}/public/*"
         ]
       },
     ]
   })
-  
+
   depends_on = [aws_s3_bucket_public_access_block.ticketea_bucket_allow_public_access]
+}
+
+resource "aws_iam_user" "ticketea-bucket-user" {
+  name = "${var.s3_bucket_name}-user"
+}
+
+resource "aws_iam_user_policy" "ticketea-bucket-user-policy" {
+  name = "${var.s3_bucket_name}-user-policy"
+  user = aws_iam_user.ticketea-bucket-user.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:*"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
