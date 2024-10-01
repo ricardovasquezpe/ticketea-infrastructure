e6191e1f60017b2da4f0b49f61226595fb31fea7
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2024-10-01 17:06:09 -0500

    fix

--- resources/ticketea-userdata.sh
+++ resources/ticketea-userdata.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# sudo apt-get update -y
-# sudo apt-get install -y docker.io
-# sudo systemctl start docker
-# sudo systemctl enable docker
-
-# docker login -u ricardovasquezpe -p Ajinomoto123@
-# docker pull ricardovasquezpe/ticketea:latest
-# docker run -d -p 3000:3000 ricardovasquezpe/ticketea:latest
-
-# curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-# docker-compose --version
-
-# git clone https://github.com/ricardovasquezpe/ticketea-infrastructure.git
-# cd ticketea-infrastructure
-# docker-compose up -d
-
-
 sudo apt update
 sudo apt upgrade -y
 
